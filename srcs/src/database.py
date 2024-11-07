8bbbdd810aaf66b0c733d7cda88cfcfaf03cd0ba
axel <lukongo.axel03@gmail.com>
2024-11-07 11:17:06 +0100

    test: still testing a new way to initialize the SQLALCHEMY_DATABASE_URL

--- srcs/src/database.py
+++ srcs/src/database.py
@@ -4,15 +4,19 @@ from sqlalchemy.orm import sessionmaker
 
 # SQLALCHEMY_DATABASE_URL = "postgresql://user_test:password_test@db/mydatabase_test"
 # SQLALCHEMY_DATABASE_URL = "postgresql://friend_db:friendpsw@postgres-service:5432/postgre_auth"
-SQLALCHEMY_DATABASE_URL = "postgresql://friend_db:friendpsw@localhost:5432/postgre_auth"
-# import os
+# SQLALCHEMY_DATABASE_URL = "postgresql://friend_db:friendpsw@localhost:5432/postgre_auth"
+import os
 
 # On lit l'URL de la base de données depuis une variable d'environnement,
 # avec une valeur par défaut au cas où la variable n'est pas définie
-# SQLALCHEMY_DATABASE_URL = os.getenv(
-#     "DATABASE_URL",
-#     "postgresql://friend_db:friendpsw@postgres-service:5432/postgre_auth"
-# )
+url = os.getenv("DATABASE_URL")
+
+if url:
+    SQLALCHEMY_DATABASE_URL = url
+else:
+    SQLALCHEMY_DATABASE_URL = "postgresql://friend_db:friendpsw@postgres-service:5432/postgre_auth"
+
+
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
